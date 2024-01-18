f39214b7dfaf3dff59bfe4bd83cf75af9d9bf9cc
alex <al3xback@gmail.com>
2024-01-18 18:25:22 +0700

    added first commit

---
+++ spec/mainSpec.js
@@ -0,0 +1,48 @@
+const { test } = QUnit;
+
+QUnit.module('DOM', (hooks) => {
+	hooks.beforeEach(() => {
+		document.body.insertAdjacentHTML('afterbegin', getBodyContent());
+	});
+
+	hooks.afterEach(() => {
+		document.body.removeChild(document.getElementById('wrapper'));
+	});
+
+	test('should have a string type of card mark content element', (assert) => {
+		const cardMarkContent =
+			document.querySelector('.card__mark').textContent;
+
+		assert.equal(typeof cardMarkContent, 'string');
+	});
+
+	test("should have a title element that equals 'Join our community' word in first section element", (assert) => {
+		const sectionEls = document.querySelectorAll('section');
+		const firstSectionEl = sectionEls[0];
+		const cardTitleEl = firstSectionEl.querySelector('.card__title');
+		const cardTitle = cardTitleEl.textContent.trim();
+
+		assert.equal(cardTitle, 'Join our community');
+	});
+
+	test('should have three section elements', (assert) => {
+		const sectionEls = document.querySelectorAll('section');
+
+		assert.strictEqual(sectionEls.length, 3);
+	});
+
+	test("should have a word 'Coding exercises' as one of why us points", (assert) => {
+		const whyUsPointEls = document.querySelectorAll('.card__list li');
+
+		const whyUsPoints = [];
+
+		for (let i = 0; i < whyUsPointEls.length; i++) {
+			const whyUsPoint = whyUsPointEls[i].textContent;
+			whyUsPoints.push(whyUsPoint);
+		}
+
+		const isExpectedWordExist = whyUsPoints.includes('Coding exercises');
+
+		assert.true(isExpectedWordExist);
+	});
+});
